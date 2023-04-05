892fb9109644f7fbb4a279d50158f26e67089967
donghapark <donghaha12@naver.com>
2023-04-05 19:47:34 +0900

    2.py

--- ans/2.py
+++ ans/2.py
@@ -1,4 +1,21 @@
 # 2. 
+# 멋진 팀원들의 **이름과 학과, 학과** 가 궁금하지않나요?<br>
+# 다음과 같이 3가지(이름, 학과, 학번)를 물어본 후  문장으로 출력하는 간단한 코드를 짜봅시다!
+
+student1={'name':'전주원','major':"컴공",'num':"21학번"}
+student2={'name':'배희은','major':"문콘경",'num':"20학번"}
+student3={'name':'김기정','major':"컴공",'num':"21학번"}
+student4={'name':'박동하','major':"정통",'num':"19 학번 "}
+
+print(student1)
+print(student2)
+print(student3)
+print(student4)
+
+
+
+
+
 
 
     
