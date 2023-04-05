86676088907000597136d56415a95b6ee414fe35
jwn <jeonjwn@naver.com>
2023-04-05 21:13:37 +0900

    6번 완료

--- ans/6.py
+++ ans/6.py
@@ -1,8 +1,24 @@
 # 6.
 
+class Movie:
+    def __init__(self, name, genre, produce):
+        self.name = name
+        self.genre = genre
+        self.produce = produce
 
-
-
-        
-
+    def new(self, name, genre, produce):
+        self.name = name
+        self.genre = genre
+        self.produce = produce
     
+    def show_name(name):
+        print(f"영화의 이름은 {name} 입니다.")
+
+
+class Cinema:
+    def __init__(self):
+        self.cinema_list=['CGV', 'MEGABOX', 'LOTTE']
+
+    def add(self,newone):
+        self.cinema_list.append(newone)
+        print(self)
