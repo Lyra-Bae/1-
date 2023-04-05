5f1558a62a5543fd4ef7bee0b2aeed7b20b3570a
jwn <jeonjwn@naver.com>
2023-04-05 20:52:38 +0900

    3번 완료

--- ans/3.py
+++ ans/3.py
@@ -1,11 +1,24 @@
 # 3. 
 
-students={
-    
+students = {
+    "전주원" : ["노래", "피아노"],
+    "김기정" : ["영화", "음주"],
+    "박동하" : ["운동", "게임"],
+    "배희은" : ["시골", "게임"]
 }
 
+show = False
 
-    
-    
+while not show:
+    name_input = input("이름을 입력하세요: ")
 
+    print(students[f"{name_input}"])
+    continue_input = input("계속하시겠습니까?(y/n): ")
+    continue_input.lower()
 
+    if continue_input == 'y':
+        pass
+    elif continue_input == 'n':
+        exit()
+    else:
+        print('\n입력 값을 확인해주세요.')
